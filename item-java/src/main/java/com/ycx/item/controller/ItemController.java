e8d63bc06db750d4c34ed9c857e7e005a46c7306
yangcxPusto <619980817@qq.com>
2022-07-18 02:13:59 +0100

    ⭐item-java add TaskExecuter as concurr

--- item-java/src/main/java/com/ycx/item/controller/ItemController.java
+++ item-java/src/main/java/com/ycx/item/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.ycx.item.entity.Item;
 import com.ycx.item.exception.ResourceNotFoundException;
 import com.ycx.item.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +19,60 @@ public class ItemController {
     @Autowired
     ItemRepository itemRepository;
 
+    private final TaskExecutor taskExecutor;
+
+    public ItemController(final TaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
+
     // Get All Items
     @GetMapping("/items")
     public List<Item> getAllItems() {
+
+        taskExecutor.execute(() -> {
+            System.out.println("get all");
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                System.out.println(e.toString());
+            }
+            System.out.println("get all");
+        });
+
         return itemRepository.findAll();
     }
 
     // Create a new Item
     @PostMapping("/items")
     public Item createItem(@Valid @RequestBody Item item) {
+
+        taskExecutor.execute(() -> {
+            System.out.println("post " + item.getId());
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                System.out.println(e.toString());
+            }
+            System.out.println("post " + item.getId());
+        });
+
         return itemRepository.save(item);
     }
 
     // Get a Single Item
     @GetMapping("/items/{id}")
     public Item getItemById(@PathVariable(value = "id") Long itemId) {
+
+        taskExecutor.execute(() -> {
+            System.out.println("get " + itemId);
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                System.out.println(e.toString());
+            }
+            System.out.println("get " + itemId);
+        });
+
         return itemRepository.findById(itemId)
                 .orElseThrow(() -> new ResourceNotFoundException("Item", "id", itemId));
     }
@@ -41,6 +81,16 @@ public class ItemController {
     @PutMapping("/items/{id}")
     public Item updateItem(@PathVariable(value = "id") Long itemId,
                            @Valid @RequestBody Item itemDetails) {
+
+        taskExecutor.execute(() -> {
+            System.out.println("put " + itemId);
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                System.out.println(e.toString());
+            }
+            System.out.println("put " + itemId);
+        });
 
         Item item = itemRepository.findById(itemId)
                 .orElseThrow(() -> new ResourceNotFoundException("Note", "id", itemId));
@@ -55,6 +105,17 @@ public class ItemController {
     // Delete a Item
     @DeleteMapping("/items/{id}")
     public ResponseEntity<?> deleteItem(@PathVariable(value = "id") Long itemId) {
+
+        taskExecutor.execute(() -> {
+            System.out.println("delete " + itemId);
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                System.out.println(e.toString());
+            }
+            System.out.println("delete " + itemId);
+        });
+
         Item item = itemRepository.findById(itemId)
                 .orElseThrow(() -> new ResourceNotFoundException("Note", "id", itemId));
 
